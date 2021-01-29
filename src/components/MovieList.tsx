1b9e1bc9299bab0834aa8abced1f703fe2bd5e0f
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-01-29 17:22:51 +0200

    Render lists of newest and most popular movies

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -4,12 +4,18 @@ import { Movie } from '../types'
 
 const MovieList: React.FC = () => {
   const [topRatedmovies, setTopRatedMovies] = useState<Movie[]>([])
+  const [popularMovies, setPopularMovies] = useState<Movie[]>([])
+  const [newMovies, setNewMovies] = useState<Movie[]>([])
 
   useEffect(() => {
     const getTopRatedMovies = async () => {
       try {
         const topRatedMoviesResponse = await instance.get(`top_rated?api_key=d5e44dd33260c00852e5fd0e20c58722&language=en-US&page=1`)
         setTopRatedMovies(topRatedMoviesResponse.data.results)
+        const popularMoviesResponse = await instance.get(`popular?api_key=d5e44dd33260c00852e5fd0e20c58722&language=en-US&page=1`)
+        setPopularMovies(popularMoviesResponse.data.results)
+        const newMoviesResponse = await instance.get(`now_playing?api_key=d5e44dd33260c00852e5fd0e20c58722&language=en-US&page=1`)
+        setNewMovies(newMoviesResponse.data.results)
       } catch (error) {
         console.error(error)
       }
@@ -19,6 +25,14 @@ const MovieList: React.FC = () => {
 
   return (
     <div className="Frontpage">
+      <h2>New</h2>
+      {newMovies !== [] && newMovies.map(movie =>
+        <li>{movie.title}</li>  
+      )}
+      <h2>Most popular</h2>
+      {popularMovies !== [] && popularMovies.map(movie =>
+        <li>{movie.title}</li>  
+        )}
       <h2>Top rated movies</h2>
       {topRatedmovies !== [] && topRatedmovies.map(movie =>
         <li>{movie.title}</li>  
