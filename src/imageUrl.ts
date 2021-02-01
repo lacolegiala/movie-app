99e8ecbc826b6174ce37a9f85117899b5fe2279a
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-02-01 16:21:19 +0200

    Delete unnecessary log

--- src/imageUrl.ts
+++ src/imageUrl.ts
@@ -1,6 +1,5 @@
 
 export const createImageUrl = (poster_path: string, options: {width: number}) => {
   const posterBaseUrl = 'http://image.tmdb.org/t/p/'
-  console.log('raaaa', `${posterBaseUrl}w${options.width}${poster_path}`)
   return `${posterBaseUrl}w${options.width}${poster_path}`
 }
