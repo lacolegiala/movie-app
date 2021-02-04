196a3395eca7cbd80a99cd1944a1bf1549ce7757
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-02-04 16:47:10 +0200

    Get search results and render titles

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useQuery } from '../hooks/useQuery';
+import { apiKey, tmdbApiClient } from '../tmdbApiClient';
+import { Movie } from '../types';
 
 const SearchResults: React.FC = () => {
+  const [results, setResults] = useState<Movie[]>([])
 
-  const queryParameter = useQuery()
+  const queryParameter = useQuery().get('query')
+
+  useEffect(() => {
+    const getResults = async () => {
+      try {
+        const resultInfo = await tmdbApiClient.get(`search/movie?api_key=${apiKey}&language=en-US&query=${queryParameter}&page=1&include_adult=false`)
+        setResults(resultInfo.data.results)
+      } catch {
+        console.log('error')
+      }
+    }
+    getResults()
+  }, [queryParameter])
 
   return (
-    <div>{queryParameter.get('query')}</div>
+    <div>
+      <h1>Search results for '{queryParameter}'</h1>
+      {results.map(result => 
+        <div key={result.id}>
+          {result.title}
+        </div>  
+      )}
+    </div>
   )
 }
 
