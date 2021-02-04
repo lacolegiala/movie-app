af226b60434ea1ef6480db77b742e52c23b42663
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-02-04 10:50:45 +0200

    Move movielist stuff to app and delete movielist

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
 import './App.css';
-import MovieList from './components/MovieList'
+import { Route, Router, Switch } from 'react-router-dom'
+import { createBrowserHistory } from 'history';
+import MovieFullInfo from './components/MovieFullInfo'
+import Home from './components/Home';
+
+const history = createBrowserHistory();
 
 function App() {
   return (
     <div className="App">
-      <MovieList />
+      <Router history={history}>
+        <Switch>
+          <Route path='/movies/:id'>
+            <MovieFullInfo />
+          </Route>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
