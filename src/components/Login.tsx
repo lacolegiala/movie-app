30e63a7ad820f28e2f7a9508c42a30c662491f0b
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-02-05 14:58:49 +0200

    Add login form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Login: React.FC = () => {
   return (
-    <h1>Login</h1>
+    <div>
+      <h1>Login</h1>
+      <form>
+        <label>
+          Username:
+          <input id='username' type='text' name='username' />
+        </label>
+        <label>
+          Password:
+          <input id='password' type='password' name='password' />
+        </label>
+      </form>
+    </div>
   )
 }
 
