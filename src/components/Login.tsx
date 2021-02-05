879b39bcd18aa3dafb117c9cfd13812f9ff69a7f
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-02-05 15:22:25 +0200

    Add event handlers to login form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login: React.FC = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleUsernameChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setUsername(event.target.value)
+  }
+
+  function handlePasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setPassword(event.target.value)
+  }
+
+  function onSubmit(event: React.ChangeEvent<HTMLFormElement>) {
+    console.log('Logged in', username, password)
+    event.preventDefault()
+  }
+
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <label>
           Username:
-          <input id='username' type='text' name='username' />
+          <input type='text' value={username} onChange={handleUsernameChange} />
         </label>
         <label>
           Password:
-          <input id='password' type='password' name='password' />
+          <input type='password' value={password} onChange={handlePasswordChange} />
         </label>
+        <button type='submit'>Login</button>
       </form>
     </div>
   )
