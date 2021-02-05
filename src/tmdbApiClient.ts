e37397cc4263142a6bf20815434aba100dae1c65
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-02-05 12:10:45 +0200

    Delete unnecessary export

--- src/tmdbApiClient.ts
+++ src/tmdbApiClient.ts
@@ -4,6 +4,7 @@ export const tmdbApiClient = axios.create({
   baseURL: 'https://api.themoviedb.org/3/'
 });
 
-export const apiKey = 'd5e44dd33260c00852e5fd0e20c58722'
+const apiKey = 'd5e44dd33260c00852e5fd0e20c58722'
+
 tmdbApiClient.defaults.params = {}
 tmdbApiClient.defaults.params['api_key'] = apiKey
