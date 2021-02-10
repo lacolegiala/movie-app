6135a85152a1dbf3e94fd1e70bee51c5c263bc7e
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-02-10 16:21:52 +0200

    Set local storage session id as query parameter to tmdbApiClient

--- src/tmdbApiClient.ts
+++ src/tmdbApiClient.ts
@@ -8,3 +8,4 @@ const apiKey = 'd5e44dd33260c00852e5fd0e20c58722'
 
 tmdbApiClient.defaults.params = {}
 tmdbApiClient.defaults.params['api_key'] = apiKey
+tmdbApiClient.defaults.params['session_id'] = window.localStorage.getItem('movie_app/sessionId')
