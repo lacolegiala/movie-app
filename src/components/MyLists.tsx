127699f6c2800751a3325bc4533074b8bfb99940
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-02-10 16:19:58 +0200

    Call account details endpoint

--- src/components/MyLists.tsx
+++ src/components/MyLists.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { tmdbApiClient } from '../tmdbApiClient'
 
 const MyLists: React.FC = () => {
+
+  useEffect(() => {
+    const getLists = async () => {
+      const accountResponse = await tmdbApiClient.get('account')
+    }
+    getLists()
+  })
+
   return (
     <div>
       <h1>My lists</h1>
