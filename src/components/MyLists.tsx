3dac3639305e9a55cfac4e3e0e7b21a95c46e826
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-02-10 16:39:11 +0200

    Call list endpoint and render list names

--- src/components/MyLists.tsx
+++ src/components/MyLists.tsx
@@ -1,18 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import { tmdbApiClient } from '../tmdbApiClient'
+import { List } from '../types'
 
 const MyLists: React.FC = () => {
+  const [lists, setLists] = useState<List[]>([])
 
   useEffect(() => {
     const getLists = async () => {
       const accountResponse = await tmdbApiClient.get('account')
+      const listResponse = await tmdbApiClient.get(`account/${accountResponse.data.id}/lists`)
+      setLists(listResponse.data.results)
     }
     getLists()
-  })
+  }, [])
 
   return (
     <div>
+      <Link to='/'>Home</Link>
       <h1>My lists</h1>
+      {lists.map(list => 
+        <li key={list.id}>{list.name}</li>  
+      )}
     </div>
   )
 }
