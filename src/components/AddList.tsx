daa301ca748a8773f0423db98da518a4d1a0ba36
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-02-11 10:37:26 +0200

    Add new list form

--- src/components/AddList.tsx
+++ src/components/AddList.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddList: React.FC = () => {
+  const [listName, setListName] = useState('')
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setListName(event.target.value)
+  }
+
   return (
     <div>
       <h1>Add a new list</h1>
+      <form>
+        Name of the list
+        <input value={listName} onChange={handleChange}/>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   )
 }
