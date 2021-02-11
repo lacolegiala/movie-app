2d99a0591dceeb7f836ac7a08a6ef840fb5ea738
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-02-11 16:45:58 +0200

    Add a base for adding movies to lists

--- src/components/MovieFullInfo.tsx
+++ src/components/MovieFullInfo.tsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { createPosterUrl } from '../imageUrl'
 import { tmdbApiClient } from '../tmdbApiClient'
-import { MovieDetails } from '../types'
+import { List, MovieDetails } from '../types'
 
 const MovieFullInfo: React.FC = () => {
   const [movie, setMovie] = useState<MovieDetails | undefined>()
+  const [lists, setLists] = useState<List[]>([])
 
   const {id} = useParams<{id: string}>()
 
@@ -14,6 +15,9 @@ const MovieFullInfo: React.FC = () => {
       try {
         const movieInfo = await tmdbApiClient.get(`movie/${id}?&language=en-US`)
         setMovie(movieInfo.data)
+        const accountResponse = await tmdbApiClient.get('account')
+        const listResponse = await tmdbApiClient.get(`account/${accountResponse.data.id}/lists`)
+        setLists(listResponse.data.results)
       } catch (error) {
         console.error(error)
       }
@@ -35,6 +39,10 @@ const MovieFullInfo: React.FC = () => {
             src={createPosterUrl(movie.poster_path, {width: 500})}
             alt='Poster of movie'
           />
+          <h2>Add to list</h2>
+          {lists.map(list => 
+            list.name  
+          )}
           <h2>Synopsis</h2> 
           <div>{movie.overview}</div>
         </div>
