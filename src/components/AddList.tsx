d69f5f78962c5f7883e4bde4fd7fef18dc86f2ba
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-02-11 11:00:19 +0200

    Call list adding endpoint when submitting add list form

--- src/components/AddList.tsx
+++ src/components/AddList.tsx
@@ -1,7 +1,27 @@
 import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { tmdbApiClient } from '../tmdbApiClient'
+
 
 const AddList: React.FC = () => {
   const [listName, setListName] = useState('')
+
+  const history = useHistory()
+  
+  const handleSubmit = async (event: React.ChangeEvent<HTMLFormElement>) => {
+    try {
+      event.preventDefault()
+      const list = {
+        name: listName,
+        description: '',
+        language: 'en'
+      }
+      await tmdbApiClient.post('list', list)
+      history.push('/lists')
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setListName(event.target.value)
@@ -10,7 +30,7 @@ const AddList: React.FC = () => {
   return (
     <div>
       <h1>Add a new list</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         Name of the list
         <input value={listName} onChange={handleChange}/>
         <button type='submit'>Submit</button>
