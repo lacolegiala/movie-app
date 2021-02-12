1b0f04743478735db6911573ec87b00df6d0eb13
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-02-12 23:36:46 +0200

    Make sure that tmdbclient always gets up-to-date version of session id

--- src/tmdbApiClient.ts
+++ src/tmdbApiClient.ts
@@ -8,4 +8,11 @@ const apiKey = 'd5e44dd33260c00852e5fd0e20c58722'
 
 tmdbApiClient.defaults.params = {}
 tmdbApiClient.defaults.params['api_key'] = apiKey
-tmdbApiClient.defaults.params['session_id'] = window.localStorage.getItem('movie_app/sessionId')
+
+tmdbApiClient.interceptors.request.use(function (config) {
+  config.params['session_id'] = window.localStorage.getItem('movie_app/sessionId')
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
+
