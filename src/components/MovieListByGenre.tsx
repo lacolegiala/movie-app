f3d6893113f5e4df4380da85fd8584dbcd5ba6b1
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-02-17 14:34:03 +0200

    List the movies of the selected genre

--- src/components/MovieListByGenre.tsx
+++ src/components/MovieListByGenre.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import { createPosterUrl } from '../imageUrl'
 import { tmdbApiClient } from '../tmdbApiClient'
 import { Movie } from '../types'
 
@@ -11,6 +12,7 @@ const MovieListByGenre: React.FC = () => {
     const discoverMovies = async () => {
       try {
         const moviesWithGenreResponse = await tmdbApiClient.get(`discover/movie?&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${id}`)
+        setMovies(moviesWithGenreResponse.data.results)
       } catch (error) {
         console.log(error)
       }
@@ -21,6 +23,17 @@ const MovieListByGenre: React.FC = () => {
   return (
     <div>
       <h1>Movies of the selected genre</h1>
+      {movies.map(movie => 
+        <div key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>
+            <h2>{movie.title}</h2>
+            <img
+              src={createPosterUrl(movie.poster_path, {width: 300})}
+              alt='Poster of movie'
+            /> 
+          </Link> 
+        </div>  
+      )}
     </div>
   )
 }
