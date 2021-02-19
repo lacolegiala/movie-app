a0639dcbdef582185224987cb55189eaef9fac71
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-02-19 18:58:58 +0200

    Add pictures to all genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,6 +5,22 @@ import { Genre } from '../types'
 import action from '../images/action.png'
 import adventure from '../images/adventure.png'
 import animation from '../images/animation.png'
+import comedy from '../images/comedy.png'
+import crime from '../images/crime.png'
+import documentary from '../images/documentary.png'
+import drama from '../images/drama.png'
+import family from '../images/family.png'
+import fantasy from '../images/fantasy.png'
+import history from '../images/history.png'
+import horror from '../images/horror.png'
+import music from '../images/music.png'
+import mystery from '../images/mystery.png'
+import romance from '../images/romance.png'
+import scienceFiction from '../images/science-fiction.png'
+import tvMovie from '../images/tv-movie.png'
+import thriller from '../images/thriller.png'
+import war from '../images/war.png'
+import western from '../images/western.png'
 
 const GenreList: React.FC = () => {
   const [genres, setGenres] = useState<Genre[]>([])
@@ -21,7 +37,7 @@ const GenreList: React.FC = () => {
     getGenres()
   }, [])
 
-  const imageChecker = (id: number) => {
+  const getImageForGenre = (id: number) => {
     switch (id) {
       case 28:
         return action
@@ -29,6 +45,38 @@ const GenreList: React.FC = () => {
         return adventure
       case 16:
         return animation
+      case 35:
+        return comedy
+      case 80:
+        return crime
+      case 99:
+        return documentary
+      case 18:
+        return drama
+      case 10751:
+        return family
+      case 14:
+        return fantasy
+      case 36:
+        return history
+      case 27:
+        return horror
+      case 10402:
+        return music
+      case 9648:
+        return mystery
+      case 10749:
+        return romance
+      case 878:
+        return scienceFiction
+      case 10770:
+        return tvMovie
+      case 53:
+        return thriller
+      case 10752:
+        return war
+      case 37:
+        return western
     }
   }
 
@@ -37,7 +85,7 @@ const GenreList: React.FC = () => {
       <h2>Genres</h2>
       <div className='GenreList'>
         {genres.map(genre =>
-          <Link className='Genre' to={`/genre/${genre.id}`} key={genre.id} style={{backgroundImage: `url(${imageChecker(genre.id)})`}}>
+          <Link className='Genre' to={`/genre/${genre.id}`} key={genre.id} style={{backgroundImage: `url(${getImageForGenre(genre.id)})`}}>
             {genre.name}
           </Link>
         )}
