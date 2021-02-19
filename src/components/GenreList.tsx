128e5612b0ed89a6dd4e7cfdd40f84a06cbaa2cf
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-02-19 17:33:22 +0200

    Add 2 more pictures to genre buttons and remove underlining from text

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import { tmdbApiClient } from '../tmdbApiClient'
 import { Genre } from '../types'
 import action from '../images/action.png'
+import adventure from '../images/adventure.png'
+import animation from '../images/animation.png'
 
 const GenreList: React.FC = () => {
   const [genres, setGenres] = useState<Genre[]>([])
@@ -19,12 +21,23 @@ const GenreList: React.FC = () => {
     getGenres()
   }, [])
 
+  const imageChecker = (id: number) => {
+    switch (id) {
+      case 28:
+        return action
+      case 12:
+        return adventure
+      case 16:
+        return animation
+    }
+  }
+
   return (
     <div>
       <h2>Genres</h2>
       <div className='GenreList'>
         {genres.map(genre =>
-          <Link className='Genre' to={`/genre/${genre.id}`} key={genre.id} style={{backgroundImage: `url(${action})`}}>
+          <Link className='Genre' to={`/genre/${genre.id}`} key={genre.id} style={{backgroundImage: `url(${imageChecker(genre.id)})`}}>
             {genre.name}
           </Link>
         )}
