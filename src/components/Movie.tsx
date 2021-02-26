b821d6d274f2f7ecd33da49f30647f7041a026de
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-02-26 17:01:57 +0200

    Cancel last commit because it put scroll buttons to wrong place (did not notice before)

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -14,21 +14,21 @@ const MovieInfo: React.FC<MovieProps> = (props: MovieProps) => {
   return (
     <div>
       <h2>{props.title}</h2>
-      <div ref={movieListElement} className='MovieList'>
-      <button className='ScrollButton' onClick={() => movieListElement.current?.scrollBy({left: -180, behavior: 'smooth'})}>Previous</button>
-      <button className='ScrollButton' onClick={() => movieListElement.current?.scrollBy({left: 180, behavior: 'smooth'})}>Next</button>
-        {props.movies.map(movie =>
-          <div className='MovieCard' key={movie.id}> 
-            <Link to={`/movies/${movie.id}`}>
-              <img
-                src={createPosterUrl(movie.poster_path, {width: 185})}
-                alt='Poster of movie'
-                className='Poster'
-              /> 
-              <h3>{movie.title} ({new Date (movie.release_date).getFullYear()})</h3>
-            </Link> 
-          </div>
-        )}
+        <button className='ScrollButton' onClick={() => movieListElement.current?.scrollBy({left: -180, behavior: 'smooth'})}>Previous</button>
+        <button className='ScrollButton' onClick={() => movieListElement.current?.scrollBy({left: 180, behavior: 'smooth'})}>Next</button>
+        <div ref={movieListElement} className='MovieList'>
+          {props.movies.map(movie =>
+            <div className='MovieCard' key={movie.id}> 
+              <Link to={`/movies/${movie.id}`}>
+                <img
+                  src={createPosterUrl(movie.poster_path, {width: 185})}
+                  alt='Poster of movie'
+                  className='Poster'
+                /> 
+                <h3>{movie.title} ({new Date (movie.release_date).getFullYear()})</h3>
+              </Link> 
+            </div>
+          )}
       </div>
     </div>
   )
