cbb91247e3823c8d3d2baa09f01998b4ecb9e092
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-03-04 14:17:25 +0200

    Handle all cases (success, loading and error) in one piece of state

--- src/components/MovieFullInfo.tsx
+++ src/components/MovieFullInfo.tsx
@@ -8,10 +8,26 @@ type MovieInfoProps = {
   sessionId: string | null
 }
 
+type Success = {
+  type: 'success'
+  data: MovieDetails
+}
+
+type Loading = {
+  type: 'loading'
+}
+
+type ErrorData = {
+  type: 'error'
+}
+
+type Case = Success | Loading | ErrorData
+
 const MovieFullInfo: React.FC<MovieInfoProps> = (props: MovieInfoProps) => {
-  const [movie, setMovie] = useState<MovieDetails | undefined>()
+  const [appCase, setAppCase] = useState<Case>()
+  
+
   const [lists, setLists] = useState<List[]>([])
-  const [error, setError] = useState(false)
 
   const {id} = useParams<{id: string}>()
 
@@ -19,14 +35,14 @@ const MovieFullInfo: React.FC<MovieInfoProps> = (props: MovieInfoProps) => {
     async () => {
       try {
         const movieInfo = await tmdbApiClient.get(`movie/${id}?&language=en-US&append_to_response=credits,videos`)
-        setMovie(movieInfo.data)
+        setAppCase({type: 'success', data: movieInfo.data})
         if (window.localStorage.getItem('movie_app/sessionId')) {
           const accountResponse = await tmdbApiClient.get('account')
           const listResponse = await tmdbApiClient.get(`account/${accountResponse.data.id}/lists`)
           setLists(listResponse.data.results)
         }
       } catch (error) {
-        setError(true)
+        setAppCase({type: 'error'})
         console.error(error)
       }
     },
@@ -39,7 +55,7 @@ const MovieFullInfo: React.FC<MovieInfoProps> = (props: MovieInfoProps) => {
 
   const addToList = async (id: number) => {
     try {
-      const movieId = movie !== undefined ? movie.id : undefined
+      const movieId = appCase?.type === 'success' ? appCase.data.id : undefined
       await tmdbApiClient.post(`list/${id}/add_item`, {media_id: movieId})
       window.alert('Added to list')
     } catch (error) {
@@ -47,21 +63,23 @@ const MovieFullInfo: React.FC<MovieInfoProps> = (props: MovieInfoProps) => {
     }
   }
 
-  const youtubeTrailer = movie?.videos.results.find(result => result.site === 'YouTube' && result.type === 'Trailer')
+  const youtubeTrailer = appCase?.type === 'success' 
+    ? appCase?.data.videos.results.find(result => result.site === 'YouTube' && result.type === 'Trailer')
+    : undefined
 
   return (
     <div className='Container'>
-      {movie !== undefined ?
+      {appCase?.type === 'success' &&
         <div>
-          <h1>{movie.title} ({new Date (movie.release_date).getFullYear()})</h1>
-          {movie.genres.map(genre =>
+          <h1>{appCase.data.title} ({new Date (appCase.data.release_date).getFullYear()})</h1>
+          {appCase.data.genres.map(genre =>
             <li key={genre.id}>
               {genre.name}
             </li>  
           )}
           <img
             className='Poster'
-            src={createPosterUrl(movie.poster_path, {width: 500})}
+            src={createPosterUrl(appCase.data.poster_path, {width: 500})}
             alt='Poster of movie'
           />
           {props.sessionId &&
@@ -73,21 +91,22 @@ const MovieFullInfo: React.FC<MovieInfoProps> = (props: MovieInfoProps) => {
             </div>
           }
           <h2>Synopsis</h2> 
-          <div>{movie.overview}</div>
+          <div>{appCase.data.overview}</div>
           {youtubeTrailer !== undefined &&
             <a href={`https://youtube.com/watch?v=${youtubeTrailer.key}`} rel='noreferrer' target='_blank'>Watch trailer</a>
           }
           <h2>Cast</h2>
           <div>
-            {movie.credits.cast.slice(0, 6).map(castMember => 
+            {appCase.data.credits.cast.slice(0, 6).map(castMember => 
               <div key={castMember.id}>
                 {castMember.name} as {castMember.character}
               </div>
             )}
           </div>
         </div>
-        : !error ? <h1>Loading</h1>
-        : 
+      }
+      {appCase?.type === 'loading' && <h1>Loading</h1>}
+      {appCase?.type === 'error' &&
         <div>
           <h1>Something went wrong. Refresh page or check that the id is correct.</h1>
           <button onClick={getMovieInfo}>Try again</button>
