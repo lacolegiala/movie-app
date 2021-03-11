18bb766600b74b9f0d80c5785b55f1efeb414e72
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-03-11 14:16:05 +0200

    Move load more button to better spot and color it

--- src/components/MovieListByGenre.tsx
+++ src/components/MovieListByGenre.tsx
@@ -51,7 +51,9 @@ const MovieListByGenre: React.FC = () => {
           </div>  
         )}
       </div>
-      <button onClick={() => setPage(page + 1)}>Load more</button>
+      <div className='ButtonContainer'>
+        <button className='LoadButton' onClick={() => setPage(page + 1)}>Load more</button>
+      </div>
     </div>
   )
 }
