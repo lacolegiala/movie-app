0f008b3a27180afa41679c9aefb853c57f7a281d
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-03-11 17:04:30 +0200

    Make search bar wider

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -9,20 +9,21 @@ type SearchBarProps = {
 const SearchBar: React.FC<SearchBarProps> = (props: SearchBarProps) => {
 
   return (
-    <form onSubmit={props.handleSubmit}>
-      <label>
-        <input
-          value={props.value} 
-          className='Input'
-          id='searchbar' 
-          type='search' 
-          name='search' 
-          placeholder='Search movies' 
-          onChange={props.handleChange}
-        />
-      </label>
-      <input type='submit' value='Search' />
-    </form>
+    <div className='SearchBar'>
+      <form onSubmit={props.handleSubmit}>
+        <div className='SearchBar'>
+          <input
+            value={props.value} 
+            className='Input'
+            id='searchbar' 
+            type='search' 
+            name='search' 
+            placeholder='Search movies' 
+            onChange={props.handleChange}
+          />
+        </div>
+      </form>
+    </div>
   )
 }
 
