6a54b6452dc8e2191882e9ddf593ae35cc5dfb3d
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-03-11 17:14:23 +0200

    Make css rules less complicated

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -11,17 +11,15 @@ const SearchBar: React.FC<SearchBarProps> = (props: SearchBarProps) => {
   return (
     <div className='SearchBar'>
       <form onSubmit={props.handleSubmit}>
-        <div className='SearchBar'>
-          <input
-            value={props.value} 
-            className='Input'
-            id='searchbar' 
-            type='search' 
-            name='search' 
-            placeholder='Search movies' 
-            onChange={props.handleChange}
-          />
-        </div>
+        <input
+          value={props.value} 
+          className='Input'
+          id='searchbar' 
+          type='search' 
+          name='search' 
+          placeholder='Search movies' 
+          onChange={props.handleChange}
+        />
       </form>
     </div>
   )
