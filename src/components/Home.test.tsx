795f74e147629604e685773806745e865abc776c
Sofia Riekkola <sofia.riekkola@gmail.com>
2025-03-11 18:09:00 +0200

    Add component tests for home view

--- src/components/Home.test.tsx
+++ src/components/Home.test.tsx
@@ -1,10 +1,19 @@
 import { render, screen } from '@testing-library/react'
 import Home from './Home'
-import { beforeEach, expect, test, vi } from 'vitest'
+import { expect, test } from 'vitest'
 
-test('renders content when not logged in', () => {
+test('renders the basic headers', () => {
   render(<Home />)
 
   const newMoviesDisplayText = screen.getByText('New')
   expect(newMoviesDisplayText).toBeDefined()
+
+  const mostPopularMoviesDisplayText = screen.getByText('Most popular')
+  expect(mostPopularMoviesDisplayText).toBeDefined()
+
+  const genreText = screen.getByText('Genres')
+  expect(genreText).toBeDefined()
+
+  const topRatedMoviesDisplayText = screen.getByText('Top rated')
+  expect(topRatedMoviesDisplayText).toBeDefined()
 })
