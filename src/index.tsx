b4c404469e0dfc6f354cf608e0c1a64405fce3d3
Sofia Riekkola <sofia.riekkola@gmail.com>
2025-03-11 17:48:47 +0200

    Refactor to work with latest react

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const domNode = document.getElementById('root');
+const root = createRoot(domNode!);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
